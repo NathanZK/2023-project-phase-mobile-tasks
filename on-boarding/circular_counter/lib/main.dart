a2de03a2998d5ec5ae9d4404f6ac49c673005d62
Nathan Kebede <nathanzelalem100@gmail.com>
2023-08-02 16:03:10 +0300

    Add Counter App

---
+++ on-boarding/circular_counter/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter = (_counter + 1) % 11;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _counter = (_counter - 1) % 11;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 50),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _increment,
+                  child: const Text('Increment'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _decrement,
+                  child: const Text("Decrement"),
+                ),
+              ],
+            )
+          ]),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Counter App"),
+          ),
+          backgroundColor: Colors.indigo.shade600,
+        ),
+        body: const Center(
+          child: Counter(),
+        ),
+      ),
+    ),
+  );
+}
